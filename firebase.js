95cf3fd9bae745d4453998f96f9825bc771c1c64
Juan Jose <juanjmm@gmail.com>
2022-01-25 21:29:17 -0600

    Firebase Config

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBohj6VCv0iU4VenzpvlKbl8Gp2a-L-1T8",
+    authDomain: "tiktok-clone-81d85.firebaseapp.com",
+    projectId: "tiktok-clone-81d85",
+    storageBucket: "tiktok-clone-81d85.appspot.com",
+    messagingSenderId: "982092753201",
+    appId: "1:982092753201:web:0ed6e7f67d6f128df0a810",
+    measurementId: "G-Z2C0KSR7Y2"
+  };
